56df6e30a47b5b5d8abc1b569006f8372fb40438
camo173 <59855386+camo173@users.noreply.github.com>
2022-04-04 15:01:43 -0400

    Update vigenere.js

--- vigenere.js
+++ vigenere.js
@@ -1,34 +1,44 @@
 async function convertToVigenere(key, input) {
-    if(key === null){ // check if the user gave a key
-      return "Missing key";
+  if(key === null){ // check if the user gave a key
+    return "Missing key";
+  }
+  key = key.toUpperCase();
+  let encrypted = "";
+  let keyArray = [];
+  let keyArrayPointer = 0; // points to the current key character value
+
+  for (let i = 0; i < key.length; i++) { // create an array out of key
+    if (key.charCodeAt(i) >= 65 && key.charCodeAt(i) <= 90) {
+      keyArray[i] = key.charCodeAt(i)-65;
     }
-    key = key.toUpperCase();
-    let encrypted = "";
-    let keyArray = [];
-    let keyArrayPointer = 0; // points to the current key character value
+  }
   
-    for (let i = 0; i < key.length; i++) { // create an array out of key
-      if (key.charCodeAt(i) >= 65 && key.charCodeAt(i) <= 90) {
-        keyArray[i] = key.charCodeAt(i)-65;
+  for (let i = 0; i < input.length; i++) {
+    
+    let tempNumber = input.charCodeAt(i);
+    
+    if ((tempNumber >= 65 &&  tempNumber <= 90)) { // entered character is a letter
+      tempNumber += keyArray[keyArrayPointer];
+      
+      if (tempNumber > 90) {
+        tempNumber -= 26;
       }
-      console.log(keyArray[i]);
+      encrypted += String.fromCharCode(tempNumber);
+
+      if (keyArrayPointer+1 <= keyArray.length-1) {
+        keyArrayPointer++;
+      }
+      else {
+        keyArrayPointer = 0;
+      }
     }
-    
-    console.log("finished converting key");
-    
-    for (let i = 0; i < input.length; i++) {
-      
-      let tempNumber = input.charCodeAt(i);
-      
-      if ((tempNumber >= 65 &&  tempNumber <= 90)) { // entered character is a letter
+      else if ((tempNumber >= 97 && tempNumber <= 122)) {
         tempNumber += keyArray[keyArrayPointer];
-        
-        if (tempNumber > 90) {
+      
+        if (tempNumber > 122) {
           tempNumber -= 26;
         }
         encrypted += String.fromCharCode(tempNumber);
-        console.log('Adding:', String.fromCharCode(tempNumber));
-        console.log(encrypted);
   
         if (keyArrayPointer+1 <= keyArray.length-1) {
           keyArrayPointer++;
@@ -37,64 +47,55 @@ async function convertToVigenere(key, input) {
           keyArrayPointer = 0;
         }
       }
-        else if ((tempNumber >= 97 && tempNumber <= 122)) {
-          tempNumber += keyArray[keyArrayPointer];
-        
-          if (tempNumber > 122) {
-            tempNumber -= 26;
-          }
-          encrypted += String.fromCharCode(tempNumber);
-          console.log('Adding:', String.fromCharCode(tempNumber));
-          console.log(encrypted);
-    
-          if (keyArrayPointer+1 <= keyArray.length-1) {
-            keyArrayPointer++;
-          }
-          else {
-            keyArrayPointer = 0;
-          }
-        }
-      else { // entered character is not a letter
-        encrypted += String.fromCharCode(tempNumber);
-        console.log('Adding:', String.fromCharCode(tempNumber));
-      }
+    else { // entered character is not a letter
+      encrypted += String.fromCharCode(tempNumber);
     }
-    return encrypted;
   }
-  
-  async function convertFromVigenere(key, input) {
-      if(key === null){ // check if the user gave a key
-      return "Missing key";
+  return encrypted;
+}
+
+async function convertFromVigenere(key, input) {
+    if(key === null){ // check if the user gave a key
+    return "Missing key";
+  }
+
+  key = key.toUpperCase();
+  let decrypted = "";
+  let keyArray = [];
+  let keyArrayPointer = 0; // points to the current key character value
+
+  for (let i = 0; i < key.length; i++) { // create an array out of key
+    if (key.charCodeAt(i) >= 65 && key.charCodeAt(i) <= 90) {
+      keyArray[i] = key.charCodeAt(i)-65;
     }
-  
-    key = key.toUpperCase();
-    let decrypted = "";
-    let keyArray = [];
-    let keyArrayPointer = 0; // points to the current key character value
-  
-    for (let i = 0; i < key.length; i++) { // create an array out of key
-      if (key.charCodeAt(i) >= 65 && key.charCodeAt(i) <= 90) {
-        keyArray[i] = key.charCodeAt(i)-65;
-      }
-      console.log(keyArray[i]);
-    }
+  }
+
+  for (let i = 0; i < input.length; i++) {
     
-    console.log("finished converting key");
-  
-    for (let i = 0; i < input.length; i++) {
+    let tempNumber = input.charCodeAt(i);
+    
+    if ((tempNumber >= 65 &&  tempNumber <= 90)) { // entered character is a letter
+      tempNumber -= keyArray[keyArrayPointer];
       
-      let tempNumber = input.charCodeAt(i);
-      
-      if ((tempNumber >= 65 &&  tempNumber <= 90)) { // entered character is a letter
+      if (tempNumber < 65) {
+        tempNumber += 26;
+      }
+      decrypted += String.fromCharCode(tempNumber);
+
+      if (keyArrayPointer+1 <= keyArray.length-1) {
+        keyArrayPointer++;
+      }
+      else {
+        keyArrayPointer = 0;
+      }
+    }
+      else if ((tempNumber >= 97 && tempNumber <= 122)) {
         tempNumber -= keyArray[keyArrayPointer];
-        console.log("Temp number is: "+ tempNumber + "\n");
-        
-        if (tempNumber < 65) {
+      
+        if (tempNumber < 97) {
           tempNumber += 26;
         }
         decrypted += String.fromCharCode(tempNumber);
-        console.log('Adding:', String.fromCharCode(tempNumber));
-        console.log(decrypted);
   
         if (keyArrayPointer+1 <= keyArray.length-1) {
           keyArrayPointer++;
@@ -103,29 +104,12 @@ async function convertToVigenere(key, input) {
           keyArrayPointer = 0;
         }
       }
-        else if ((tempNumber >= 97 && tempNumber <= 122)) {
-          tempNumber -= keyArray[keyArrayPointer];
-          console.log("Temp number is: "+ tempNumber + "\n");
-        
-          if (tempNumber < 97) {
-            tempNumber += 26;
-          }
-          decrypted += String.fromCharCode(tempNumber);
-          console.log(decrypted);
-    
-          if (keyArrayPointer+1 <= keyArray.length-1) {
-            keyArrayPointer++;
-          }
-          else {
-            keyArrayPointer = 0;
-          }
-        }
-      else { // entered character is not a letter
-        decrypted += String.fromCharCode(tempNumber);
-      }
+    else { // entered character is not a letter
+      decrypted += String.fromCharCode(tempNumber);
     }
-    return decrypted;
   }
-  
-  module.exports.convertToVigenere = convertToVigenere
-  module.exports.convertFromVigenere = convertFromVigenere
+  return decrypted;
+}
+
+module.exports.convertToVigenere = convertToVigenere
+module.exports.convertFromVigenere = convertFromVigenere
